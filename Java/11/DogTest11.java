e4c9f8d7daf4a4fd0d53cef9e65b5eff7d700a8f
MikiS <73443709+RAV64@users.noreply.github.com>
2021-03-31 19:58:07 +0300

    Name change

--- Java/11/DogTest11.java
+++ Java/11/DogTest11.java
@@ -1,8 +1,8 @@
-class Dog {
+class Dog11 {
     int age;
     String name, race, sound;
 
-    public Dog(int age, String name, String race, String sound) {
+    public Dog11(int age, String name, String race, String sound) {
         this.age = age;
         this.name = name;
         this.race = race;
@@ -20,10 +20,10 @@ class Dog {
     }
 }
 
-public class DogTest {
+public class DogTest11 {
     public static void main(String[] args) {
-        Dog rover = new Dog(2, "Rover", "Dalmatian", "Woof!!!");
-        Dog rex = new Dog(4, "Rex", "Australian terrier", "woofwoof");
+        Dog11 rover = new Dog11(2, "Rover", "Dalmatian", "Woof!!!");
+        Dog11 rex = new Dog11(4, "Rex", "Australian terrier", "woofwoof");
 
         System.out.println("Dog info:");
         rover.printInfo();
